78f6decf9b30d033f4524aed5f978ac4f1b06129
Umesh Kumar <umeshsingla05@gmail.com>
2014-10-30 17:37:09 +0530

    Revert "Add some themes of fronted"
    This reverts commit 8f32a4c495e3f504252f49f42853e26a11b2842e.

--- framework/loader.php
+++ framework/loader.php
@@ -46,10 +46,7 @@ if (!function_exists('ig_enqueue_scripts')) {
         $url = plugin_dir_url(__FILE__);
 
         wp_register_style('ig-bootstrap', $url . 'assets/bootstrap.css');
-        wp_register_style('ig-bootstrap-lumen', $url . 'assets/lumen.css');
-        wp_register_style('ig-bootstrap-flaty', $url . 'assets/flaty.css');
-        wp_register_style('ig-bootstrap-paper', $url . 'assets/paper.css');
-        wp_register_style('ig-bootstrap-united', $url . 'assets/united.css');
+        wp_register_style('ig-bootstrap-lumen', $url . 'assets/bootstrap.css');
         wp_register_script('ig-bootstrap', $url . 'assets/bootstrap.min.js', array('jquery'));
         wp_register_style('ig-fontawesome', $url . 'assets/fa/css/font-awesome.css');
     }
