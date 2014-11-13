ae488a616e99e75550348714f62fac715a1ba84b
HoangNgo <hoang@incsub.com>
2014-11-13 23:41:57 +0700

    Code to check does class exist before declare

--- app/addons/bbpress-integration.php
+++ app/addons/bbpress-integration.php
@@ -8,95 +8,97 @@
 /**
  * @author:Hoang Ngo
  */
-class MM_BBPress_Addon extends IG_Request
-{
-    private static $_instance;
-
-    public $plugin_url;
-    public $plugin_path;
-    public $domain;
-    public $prefix;
-
-    private $controller;
-
-    private function __construct()
+if (!class_exists('MM_BBPress_Addon')) {
+    class MM_BBPress_Addon extends IG_Request
     {
-        //variables init
-        $this->plugin_url = plugin_dir_url(__FILE__) . 'bbpress-intergration/';
-        $this->plugin_path = plugin_dir_path(__FILE__) . 'bbpress-intergration/';
-        $this->domain = mmg()->domain;
-        $this->prefix = mmg()->prefix;
+        private static $_instance;
 
-        spl_autoload_register(array(&$this, 'loader'));
+        public $plugin_url;
+        public $plugin_path;
+        public $domain;
+        public $prefix;
 
-        add_action('wp_enqueue_scripts', array(&$this, 'scripts'));
-        $this->controller = new MM_BBPress_Controller();
-    }
+        private $controller;
 
-    function scripts()
-    {
+        private function __construct()
+        {
+            //variables init
+            $this->plugin_url = plugin_dir_url(__FILE__) . 'bbpress-intergration/';
+            $this->plugin_path = plugin_dir_path(__FILE__) . 'bbpress-intergration/';
+            $this->domain = mmg()->domain;
+            $this->prefix = mmg()->prefix;
 
-    }
+            spl_autoload_register(array(&$this, 'loader'));
 
-    function loader($class)
-    {
-        $filename = str_replace('_', '-', strtolower($class)) . '.php';
-        if (strstr($filename, '-controller.php')) {
-            //looking in the controllers folder and sub folders to get this class
-            $files = $this->listFolderFiles($this->plugin_path . 'app/controllers');
-            foreach ($files as $file) {
-                if (strcmp($filename, pathinfo($file, PATHINFO_BASENAME)) === 0) {
-                    include_once $file;
-                    break;
-                }
-            }
-        } elseif (strstr($filename, '-model.php')) {
-            $files = $this->listFolderFiles($this->plugin_path . 'app/models');
-
-            foreach ($files as $file) {
-                if (strcmp($filename, pathinfo($file, PATHINFO_BASENAME)) === 0) {
-                    include_once $file;
-                    break;
-                }
-            }
-        } else {
-            //include normal file inside app folder
-            if (file_exists($this->plugin_path . 'app/' . $filename)) {
-                include_once $this->plugin_path . 'app/' . $filename;
-            }
+            add_action('wp_enqueue_scripts', array(&$this, 'scripts'));
+            $this->controller = new MM_BBPress_Controller();
         }
-    }
 
-    function listFolderFiles($dir)
-    {
-        $ffs = scandir($dir);
-        $i = 0;
-        $list = array();
-        foreach ($ffs as $ff) {
-            if ($ff != '.' && $ff != '..') {
-                if (strlen($ff) >= 5) {
-                    if (substr($ff, -4) == '.php') {
-                        $list[] = $dir . '/' . $ff;
+        function scripts()
+        {
+
+        }
+
+        function loader($class)
+        {
+            $filename = str_replace('_', '-', strtolower($class)) . '.php';
+            if (strstr($filename, '-controller.php')) {
+                //looking in the controllers folder and sub folders to get this class
+                $files = $this->listFolderFiles($this->plugin_path . 'app/controllers');
+                foreach ($files as $file) {
+                    if (strcmp($filename, pathinfo($file, PATHINFO_BASENAME)) === 0) {
+                        include_once $file;
+                        break;
                     }
                 }
-                if (is_dir($dir . '/' . $ff))
-                    $list = array_merge($list, $this->listFolderFiles($dir . '/' . $ff));
+            } elseif (strstr($filename, '-model.php')) {
+                $files = $this->listFolderFiles($this->plugin_path . 'app/models');
+
+                foreach ($files as $file) {
+                    if (strcmp($filename, pathinfo($file, PATHINFO_BASENAME)) === 0) {
+                        include_once $file;
+                        break;
+                    }
+                }
+            } else {
+                //include normal file inside app folder
+                if (file_exists($this->plugin_path . 'app/' . $filename)) {
+                    include_once $this->plugin_path . 'app/' . $filename;
+                }
             }
         }
-        return $list;
-    }
 
-    static function get_instance()
-    {
-        if (!is_object(self::$_instance)) {
-            self::$_instance = new MM_BBPress_Addon();
+        function listFolderFiles($dir)
+        {
+            $ffs = scandir($dir);
+            $i = 0;
+            $list = array();
+            foreach ($ffs as $ff) {
+                if ($ff != '.' && $ff != '..') {
+                    if (strlen($ff) >= 5) {
+                        if (substr($ff, -4) == '.php') {
+                            $list[] = $dir . '/' . $ff;
+                        }
+                    }
+                    if (is_dir($dir . '/' . $ff))
+                        $list = array_merge($list, $this->listFolderFiles($dir . '/' . $ff));
+                }
+            }
+            return $list;
         }
-        return self::$_instance;
-    }
-}
 
-function mm_bbi()
-{
-    return MM_BBPress_Addon::get_instance();
+        static function get_instance()
+        {
+            if (!is_object(self::$_instance)) {
+                self::$_instance = new MM_BBPress_Addon();
+            }
+            return self::$_instance;
+        }
+    }
+
+    function mm_bbi()
+    {
+        return MM_BBPress_Addon::get_instance();
+    }
 }
 mm_bbi();
