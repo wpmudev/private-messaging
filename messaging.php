4bb599f3c879e8900d67b0b80369cc8e781f7e48
HoangNgo <hoang@incsub.com>
2014-12-05 21:47:20 +0700

    Update version

--- messaging.php
+++ messaging.php
@@ -4,7 +4,7 @@ Plugin Name: Private Messaging
 Plugin URI: https://premium.wpmudev.org/project/XXXXXXX/
 Description:
 Author: WPMU DEV
-Version: 1.0 RC 5
+Version: 1.0 RC 6
 Author URI: http://premium.wpmudev.org
 WDP ID: ***
 Text Domain: private_messaging
@@ -37,7 +37,7 @@ if (!class_exists('MMessaging')) {
         public $domain;
         public $prefix;
 
-        public $version = "1.0 RC5";
+        public $version = "1.0 RC6";
 
         public $global = array();
 
