8d7bf2333137e31e97f8f306b09f479b03851f20
HoangNgo <hoang@incsub.com>
2015-02-23 13:08:23 +0700

    Update change log & version

--- messaging.php
+++ messaging.php
@@ -4,7 +4,7 @@ Plugin Name: Private Messaging
 Plugin URI: https://premium.wpmudev.org/project/private-messaging
 Description: Private user-to-user communication for placing bids, sharing project specs and hidden internal communication. Complete with front end integration, guarded contact information and protected file sharing.
 Author: WPMU DEV
-Version: 1.0.2-Beta
+Version: 1.0.1.1-Beta
 Author URI: http://premium.wpmudev.org
 WDP ID: 938495
 Text Domain: private_messaging
@@ -37,7 +37,7 @@ if (!class_exists('MMessaging')) {
         public $domain;
         public $prefix;
 
-        public $version = "1.0.2";
+        public $version = "1.0.1.1";
         public $db_version = '1.0';
 
         public $global = array();
